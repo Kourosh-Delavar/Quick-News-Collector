a6e97a55cdc2939d02ee9a1da9891017549e87bf
Kourosh <delavar.kourosh@gmail.com>
2025-05-06 21:56:45 +0330

    Saving API URLs with tokens as CSV file

--- api_source_maker.py
+++ api_source_maker.py
@@ -2,12 +2,13 @@ import pwinput
 
 def add_api(url: str, token: str, source_path: str) -> None:
     with open(source_path, 'a+') as file:
-        file.write(url + " " + token + "\n")
+        row: str = url + "," + token + ",\n"
+        file.write(row)
 
 def main() -> None:
     url: str = input("Please insert your API URL : ")
     token: str = pwinput.pwinput(prompt="Please insert your API token : ")
-    path: str = "/home/kou/projects/QNC/test.txt"
+    path: str = "/home/kou/projects/QNC/api_base.csv"
     add_api(url=url, token=token, source_path=path)
         
 if __name__ == '__main__':
